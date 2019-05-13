160a7fbbebe47727fa2d6a7e63a21aef85dda473
Edoardo <edoardo.fullin@hotmail.it>
2019-05-13 21:41:31 +0200

    Client docs download

---
+++ SharedLibrary/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedLibrary {
+    public class Test {
+        public string TestName { get; set; }
+        public TimeSpan Time { get; set; }
+        public string DataDownloadPath { get; set; }
+        public bool ReclaimTestImmediately { get; set; }
+        public bool DeleteFilesAfterEnd { get; set; }
+
+    }
+}
--- Testing_Reloaded_Client/TestManager.cs
+++ Testing_Reloaded_Client/TestManager.cs
@@ -1,26 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SharedLibrary;
 
 namespace Testing_Reloaded_Client {
     public class TestManager {
-
         private Server server;
-
-
-
 
         private TcpClient tcpClient;
         private NetworkStream dataStream;
         private StreamWriter netWriter;
         private StreamReader netReader;
 
+        private Test currentTest;
+
         public TestManager(Server server) {
             this.server = server;
             tcpClient = new TcpClient(AddressFamily.InterNetwork);
+        }
+
+        public static string ResolvePath(string path) {
+            return Environment.ExpandEnvironmentVariables(path).Replace("$cognome", Me.Surname);
         }
 
         public async Task ConnectToServer() {
@@ -39,20 +43,37 @@ namespace Testing_Reloaded_Client {
 
             await netWriter.WriteLineAsync(str);
 
-            string response;
-            JObject jsonResponse;
+            var response = await netReader.ReadLineAsync();
+            var jsonResponse = JObject.Parse(response);
 
-            while (true) {
-                response = await netReader.ReadLineAsync();
-                jsonResponse = JObject.Parse(response);
-
-                if ((int)jsonResponse.GetValue("PacketID") == packet.PacketID) break;
-            }
-
-
-
-
+            this.currentTest = JsonConvert.DeserializeObject<Test>(jsonResponse["Test"].ToString());
         }
-        
+
+        public async Task DownloadTestDocumentation() {
+            if (dataStream == null || currentTest == null) return; // TODO
+
+            string path = ResolvePath(currentTest.DataDownloadPath);
+            Directory.Delete(path, true);
+            Directory.CreateDirectory(path);
+
+            var packet = new { Action = "GetTestDocs", PacketID = Statics.GenerateRandomPacketId() };
+
+            await netWriter.WriteLineAsync(JsonConvert.SerializeObject(packet));
+            var response = await netReader.ReadLineAsync();
+
+            var jsonResponse = JObject.Parse(response);
+
+            if (jsonResponse["FileType"].ToString() == "archive") {
+
+                int fileSize = (int)jsonResponse.GetValue("FileSize");  // 2GB Limit
+                var bytes = new byte[fileSize];
+
+                await dataStream.ReadAsync(bytes, 0, bytes.Length);           
+
+                var fastZip = new FastZip();
+
+                fastZip.ExtractZip(new MemoryStream(bytes), path, FastZip.Overwrite.Always, null, null, null, true, true);
+            }
+        }
     }
 }
